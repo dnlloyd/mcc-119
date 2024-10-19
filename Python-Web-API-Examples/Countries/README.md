0f50f438f5074b3765c96bae31ed067f871259b4
Daniel Lloyd <dnlloyd@outlook.com>
2024-10-19 11:51:10 -0500

    Add web API countries example

---
+++ Python-Web-API-Examples/Countries/README.md
@@ -0,0 +1,26 @@
+# Python Web API example: Countries
+
+## 1. Initial run
+
+**Code**: [countries-initial.py](countries-initial.py)
+
+**Screen shot of output**
+
+![alt text](image-1.png)
+
+**Formatted output**: [countries_data-output.py](countries_data-output.py)
+
+## 2. Final run
+
+**Code**: [countries-final.py](countries-final.py)
+
+**Output**
+
+```
+What country would like to search for? china
+Found 4 countries
+Taiwan
+Hong Kong
+China
+Macau
+```
---
+++ Python-Web-API-Examples/Countries/countries-initial.py
@@ -0,0 +1,21 @@
+import requests
+import pprint
+
+
+# Prompt use for country and save as string variable named "country"
+country_search_pattern = input("What country would like to search for? ")
+
+# Make the GET request to the API endpoint and store response to variable named "response"
+response = requests.get(f'https://restcountries.com/v3.1/name/{country_search_pattern}')
+
+# Extract the JSON (dictionaries OR list of dictionaries) from the response and store as variable named "countries_data"
+countries_data = response.json()
+
+# Developer only (do not print this in your final code): Print the type of object "countries_data" is
+#  This will help guide your next steps. Is it a list? Is it a dictionary?
+print(f"The response is of type: {type(countries_data)}\n")
+
+# Developer only (do not print this in your final code): Pretty print the type of object "countries_data" is. This should help you understand 
+# the structure of the object you're dealing with
+print("Pretty printing countries_data:\n")
+pprint.pp(countries_data)
