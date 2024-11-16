04b8487b227a576bb85580956ba7eac293988e90
Daniel Lloyd <dnlloyd@outlook.com>
2024-11-16 15:01:46 -0600

    more powershell

--- Lecture-Examples/PowerShell/CredentialChecker/UsernameAndPasswordCheck.ps1
+++ Lecture-Examples/PowerShell/CredentialChecker/UsernameAndPasswordCheck.ps1
@@ -3,7 +3,10 @@ $SecurePassword = Read-Host "Enter passowrd" -AsSecureString
 
 $Credentials = New-Object System.Management.Automation.PSCredential ($UserName, $SecurePassword)
 
-if ($Credentials.GetNetworkCredential().Password -eq "abc123" -and $Credentials.GetNetworkCredential().UserName -eq "danl") {
+$UsernameFromCreds = $Credentials.GetNetworkCredential().UserName
+$PlainTextPassword = $Credentials.GetNetworkCredential().Password
+
+if ($UsernameFromCreds -eq "danl" -and $PlainTextPassword -eq "abc123") {
     Write-Output "Hello $($Credentials.GetNetworkCredential().UserName), you can now access the system."
 } else {
     Write-Host "System denied!"
